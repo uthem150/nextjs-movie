2b772cdaae74addb5ca73a2834e0e520d7aa6684
uthem150 <uthem150@gmail.com>
2024-09-12 00:21:13 +0900

    add. hydration 관련 주석 추가

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,4 +1,5 @@
-"use client";
+"use client"; //client에서 interactive하게 만들어질(hydrate될) components는 use client지시어를 맨 위에 가져야 함 -> 의미는 use hydrate
+// use client코드가 없으면, 자바스크립트를 그만큼 다운받지 않아도 됨 -> 속도 상승
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
