0c2ea3c828a93e0533e299b17d7a17d025629701
uthem150 <uthem150@gmail.com>
2024-09-23 23:05:19 +0900

    props를 안에 넣으면, 두 종류의 props를 얻을 수 있음

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,6 +1,11 @@
 // [] <- 대괄호 폴더를 만들면, 동적 라우팅을 할 수 있음.
 // movies/1212
 
-export default function MovieDetail() {
+// props를 안에 넣으면, 두 종류의 props를 얻을 수 있음
+// http://localhost:3000/movies/111?region=kr&page=2
+// -> { params: { id: '111' }, searchParams: { region: 'kr', page: '2' } }
+
+export default function MovieDetail(props) {
+  console.log(props);
   return <h1>Movie</h1>;
 }
