66bfd44ec4c24987dd333d2d6afc0537a9522e53
uthem150 <uthem150@gmail.com>
2024-09-24 08:06:26 +0900

    fetch는 항상 client에서 일어남(브라우저가 API에 요청을 보냄) -> 보안상의 문제 발생 가능

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,18 +4,21 @@ import { useEffect, useState } from "react";
 // client component에서는 metadata를 export할 수 없음.
 
 export default function Page() {
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const getMovies = async () => {
     const response = await fetch(
       "https://nomad-movies.nomadcoders.workers.dev/movies"
     );
     const json = await response.json();
     setMovies(json);
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getMovies();
   }, []);
 
-  return <div>{JSON.stringify(movies)} </div>;
+  return <div>{isLoading ? "Loading" : JSON.stringify(movies)} </div>;
 }
