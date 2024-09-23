e27e7d736e23078f162192da3b0ed1a425be6d2c
uthem150 <uthem150@gmail.com>
2024-09-23 22:41:17 +0900

    메타 데이터는 병합됨(중첩도 가능하긴 함)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import Navigation from "../components/navigation";
 
+export const metadata = {
+  description: "Next.js로 만든 무비 웹",
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
