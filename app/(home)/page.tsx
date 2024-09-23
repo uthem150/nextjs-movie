29c61439a8533c06b08d0631f3315702fd8541fe
uthem150 <uthem150@gmail.com>
2024-09-24 08:01:31 +0900

    client side에서 데이터 fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,21 @@
-export const metadata = {
-  title: "Home", // 브라우저 탭에 보이는 page title
-};
+"use client";
+import { useEffect, useState } from "react";
+
+// client component에서는 metadata를 export할 수 없음.
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{JSON.stringify(movies)} </div>;
 }
