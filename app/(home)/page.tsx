cb979e34c32396b3fed2c4d7665364123fdfaf83
uthem150 <uthem150@gmail.com>
2024-09-23 22:40:03 +0900

    메타 데이타 사용

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Home | Next Movies", // 브라우저 탭에 보이는 page title
+  description: "Next.js로 만든 무비 웹",
+};
+
 export default function Page() {
   return (
     <div>
