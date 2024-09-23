f10de2407e663d48186bc4c351323868a258ce24
uthem150 <uthem150@gmail.com>
2024-09-24 08:12:04 +0900

    NextJS에서 다르게 데이터 받아오는 방식

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,15 @@ import { useEffect, useState } from "react";
 
 // client component에서는 metadata를 export할 수 없음.
 
+
+// React는 useState, setMovies, loading상태 관리 등을 모두 해줘야 함
+// nextJS는 서버 컴포넌트에서, 할 수 있으므로 -> useEffect, useState를 쓰지 않아도 됨 + 로딩상태 구현 안해도 됨
+// 프레임워크가 대신 해줌 -> 모든 것은 서버 컴포넌트로부터 비롯됨 (api도 안전하게 숨길 수 있게 됨)
+// useState를 쓰면 metadata도 쓸 수 없는데, 서버 컴포넌트에서 하면 됨.
+
+// NextJS와 서버컴포넌트를 이용하면 React App <-----> API <-----> DB 흐름이 아니라,
+// API가 필요가 없게 됨.
+
 export default function Page() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
