b148c7df36daf689c740199cf6ae1f746692e82b
uthem150 <uthem150@gmail.com>
2024-09-28 16:59:20 +0900

    grid style 적용

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,13 @@
-import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
   title: "Home",
 };
 
-// client component에서는 metadata를 export할 수 없음.
-
-// React는 useState, setMovies, loading상태 관리 등을 모두 해줘야 함
-// nextJS는 서버 컴포넌트에서, 할 수 있으므로 -> useEffect, useState를 쓰지 않아도 됨 + 로딩상태 구현 안해도 됨
-// 프레임워크가 대신 해줌 -> 모든 것은 서버 컴포넌트로부터 비롯됨 (api도 안전하게 숨길 수 있게 됨)
-// useState를 쓰면 metadata도 쓸 수 없는데, 서버 컴포넌트에서 하면 됨.
-
-// NextJS와 서버컴포넌트를 이용하면 React App <-----> API <-----> DB 흐름이 아니라,
-// API가 필요가 없게 됨.
-
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-
   return fetch(API_URL).then((response) => response.json());
 }
 
@@ -26,11 +15,14 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
