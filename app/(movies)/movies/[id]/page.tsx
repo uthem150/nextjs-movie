1e38b1abd2701931a40101e6c34ff29d5b067139
uthem150 <uthem150@gmail.com>
2024-09-29 15:17:17 +0900

    동적인 메타데이터 제목을 갖고 있는 페이지 생성 (generateMetadata)

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,19 +2,27 @@
 // movies/1212
 
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+interface IParams {
+  params: { id: string };
+}
+
+// 동적인 제목을 갖고 있는 페이지를 위해 사용
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 // props를 안에 넣으면, 두 종류의 props를 얻을 수 있음
 // http://localhost:3000/movies/111?region=kr&page=2
 // -> { params: { id: '111' }, searchParams: { region: 'kr', page: '2' } }
 
 // params: { id }는 props의 params 객체에서 id 속성을 추출하는 방식
-export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   // http://localhost:3000/movies/111
   // Movie 111이 화면에 나오게 됨
 
